f164228f9e94e54aba102c0ac824143ca8d851db
Samprex2s1 <92262036+Samprex2s1@users.noreply.github.com>
2021-11-02 23:00:26 +0600

    Add files via upload

---
+++ dedes.py
@@ -0,0 +1,8 @@
+alpha = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+n = int(input())
+s = input().strip()
+res = ''
+for c in s:
+    res += alpha[(alpha.index(c) - n) % len(alpha)]
+print('Result: "' + res + '"')
+input()
