3c716889f1d4296948217dab38afe2fe0fbcaadb
PlutoAA <uuuuuuurt@mail.ru>
2021-11-15 21:37:28 +0600

    GET API

--- rasp/main/views.py
+++ rasp/main/views.py
@@ -3,7 +3,9 @@ from django.views.generic import TemplateView
 from .models import Task
 from .forms import TaskForm
 import json
+import requests
 from django.http import JsonResponse
+from django.template.loader import get_template
 
 # Create your views here.
 def index(request):
@@ -30,6 +32,13 @@ def create(request):
     return render(request,'main/create.html', conttext)
 
 def table(request):
-    with open("main/fixtures/data.json", encoding='utf-8') as json_file:
-        data = json.load(json_file)
-    return render(request,'main/table.html', data)
+    start='2021.11.08'
+    finish='2021.11.14'
+    lng=1
+    response = requests.get('https://rasp.omgtu.ru/api/schedule/person/1012', params={
+    "start": start,
+    "finish": finish,
+    "lng": lng
+    })
+    data = response.json()
+    return get_template('main/table.html').render(data, request=request)
