0dd58307503e82cf9fdc8666b18693965848234a
PlutoAA <uuuuuuurt@mail.ru>
2021-11-15 12:51:34 +0600

    update beutiful  table

--- rasp/main/views.py
+++ rasp/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
+import json
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
@@ -29,3 +31,16 @@ def create(request):
 
 def table(request):
     return render(request,'main/table.html')
+
+def json_r(request):
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+
+def profile(request):
+    data = {
+        'name': 'Vitor',
+        'location': 'Finland',
+        'is_active': True,
+        'count': 28
+    }
+    return JsonResponse(data)
