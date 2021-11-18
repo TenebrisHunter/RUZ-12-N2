10c5ae0d28eb57212f1b35b1b49c9e95ff5c2c34
PlutoAA <uuuuuuurt@mail.ru>
2021-11-18 13:51:03 +0600

    table edit

--- rasp/main/views.py
+++ rasp/main/views.py
@@ -31,8 +31,8 @@ def create(request):
     return render(request,'main/create.html', conttext)
 
 def table(request):
-    start='2021.11.08'
-    finish='2021.11.14'
+    start='2021.11.16'
+    finish='2021.11.16'
     lng=1
     response = requests.get('https://rasp.omgtu.ru/api/schedule/person/1012', params={
     "start": start,
