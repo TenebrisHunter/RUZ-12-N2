3231c45e0355fff6cdb177325f7bd5fa3f336c62
PlutoAA <uuuuuuurt@mail.ru>
2021-11-20 03:06:59 +0600

    ready database without dupes

--- new.py
+++ new.py
@@ -5,18 +5,29 @@ import json
 conn = sqlite3.connect('data.db')
 cursor = conn.cursor()
 
-response = requests.get('https://rasp.omgtu.ru/api/search?term=а&type=person')
-data = response.json()
-
 cursor.executescript('''
 CREATE TABLE if not exists teachers (id varchar(8000), description text, label text, type text)
 ''')
 
-for teacher in data:
-    cursor.execute(f"SELECT label FROM teachers WHERE id = '{id}' ")
-    if cursor.fetchone() is None:
-        cursor.execute(f"insert into teachers values (?, ?, ?, ?)",
-                        [teacher['id'], teacher['description'], teacher['label'],teacher['type']])
-        conn.commit()
-    else:
-        pass
+def check():
+    data = response.json()
+    for teacher in data:
+        cursor.execute(f"SELECT label FROM teachers WHERE id = {teacher['id']} ")
+        if cursor.fetchone() is None:
+            cursor.execute(f"insert into teachers values (?, ?, ?, ?)",
+                            [teacher['id'], teacher['description'], teacher['label'],teacher['type']])
+            conn.commit()
+        else:
+            pass
+
+dict = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+
+for i in dict:
+    response = requests.get('https://rasp.omgtu.ru/api/search?term='+i+'&type=person')
+    check()
+    for o in dict:
+        response = requests.get('https://rasp.omgtu.ru/api/search?term='+i+o+'&type=person')
+        check()
+        for p in dict:
+            response = requests.get('https://rasp.omgtu.ru/api/search?term='+i+o+p+'&type=person')
+            check()
