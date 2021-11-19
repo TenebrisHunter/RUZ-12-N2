3706910bc41e0347aaccec87b40f4df5e9488e50
PlutoAA <uuuuuuurt@mail.ru>
2021-11-20 00:14:50 +0600

    proverka unikalnosti

--- new.py
+++ new.py
@@ -5,7 +5,7 @@ import json
 conn = sqlite3.connect('data.db')
 cursor = conn.cursor()
 
-response = requests.get('https://rasp.omgtu.ru/api/search?term=в&type=person')
+response = requests.get('https://rasp.omgtu.ru/api/search?term=а&type=person')
 data = response.json()
 
 cursor.executescript('''
@@ -13,6 +13,10 @@ CREATE TABLE if not exists teachers (id varchar(8000), description text, label t
 ''')
 
 for teacher in data:
-    cursor.execute("insert into teachers values (?, ?, ?, ?)",
-                    [teacher['id'], teacher['description'], teacher['label'],teacher['type']])
-    conn.commit()
+    cursor.execute(f"SELECT label FROM teachers WHERE id = '{id}' ")
+    if cursor.fetchone() is None:
+        cursor.execute(f"insert into teachers values (?, ?, ?, ?)",
+                        [teacher['id'], teacher['description'], teacher['label'],teacher['type']])
+        conn.commit()
+    else:
+        pass
