63b8ae06bf65449cbee8c488b92c1204dff4630c
Misha Klyuchenko <misha.klyuchenko@gmail.com>
2023-05-13 01:14:37 +0600

    initial

--- rasp/settings.py
+++ rasp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-kbg+((ee9+w=2r(b2cz^b+!t8w_fqt!olk*n2#d+woor86vtnn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
